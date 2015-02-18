d6a2683f870c42798b3248b331c6ae42833165f3
Timur Kalimov <tkalimov@gmail.com>
2015-02-18 12:21:36 -0500

    turn on debug for production temporarily

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'mf^#^j&u^#18!6ys8oioc4re6ge^d7++lfap-wa%q13x(sz@02'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
